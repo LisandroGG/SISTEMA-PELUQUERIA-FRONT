ab0a1cb74b0ecd173203665d18381780faea73f7
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2025-08-22 20:10:18 -0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -6,20 +6,42 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 
+function Root() {
+useEffect(() => {
+console.log("⏱ React montado", performance.now());
+  const loader = document.getElementById("loading");
+  const root = document.getElementById("root");
+
+  if (loader) {
+    loader.style.opacity = "0";
+    setTimeout(() => loader.remove(), 300);
+  }
+
+  if (root) {
+    root.style.opacity = "1"; // ahora se muestra React
+  }
+}, []);
+
+  return <App />;
+}
+
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Root />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
 
-window.addEventListener("load", () => {
-	const loader = document.getElementById("loading");
-	if (loader) {
-    loader.style.opacity = "0";
-    setTimeout(() => loader.remove());
-	}
+console.log("⏱ Inicio de script", performance.now());
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("⏱ DOM listo", performance.now());
 });
+
+window.addEventListener("load", () => {
+  console.log("⏱ Recursos cargados", performance.now());
+});
+
